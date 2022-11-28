8933d2c81751e1c359031044bb027dd88ed52570
hak.dok0@gmail.com <hak.dok0@gmail.com>
2022-11-28 12:10:28 +0300

    Add super user mode

--- CpuGUI/MainWindow.xaml.cs
+++ CpuGUI/MainWindow.xaml.cs
@@ -1,21 +1,8 @@
 ﻿using CPUConsole;
-using CPUConsole.Commands.Flow;
 using CPUConsole.Memory;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CpuGUI
 {
@@ -26,6 +13,7 @@ namespace CpuGUI
     {
         CPU cpu;
         Parser p;
+        bool superUser = false;
         public MainWindow()
         {
             cpu = new CPU();
@@ -40,7 +28,7 @@ namespace CpuGUI
             codeTextBox.Text = p.textCode;
             cpu.AddCommands(p.GetCommands(codeTextBox.Text));
             cpu.ExecuteCommands();
-            
+
 
         }
 
@@ -53,12 +41,12 @@ namespace CpuGUI
             foreach (var reg in cpu.registers.Flags)
                 flagsListBox.Items.Add($"{reg.Key}[{reg.Value}]");
             ProgramCounterLabel.Content = cpu.registers.ProgrammCounter;
-            
+
         }
 
-         public void UpdateRegView()
+        public void UpdateRegView()
         {
-           for(int i = 0; i< cpu.registers.Integer.Length; i++)
+            for (int i = 0; i < cpu.registers.Integer.Length; i++)
                 intRegListBox.Items[i] = cpu.registers.Integer[i];
 
             for (int i = 0; i < cpu.registers.Float.Length; i++)
@@ -72,7 +60,7 @@ namespace CpuGUI
             }
 
             ProgramCounterLabel.Content = cpu.registers.ProgrammCounter;
-            
+
         }
 
         public void UpdateButtonVisible()
@@ -87,6 +75,7 @@ namespace CpuGUI
                 prevExecuteCommandButton.IsEnabled = true;
                 startCpuButton.Content = "Restart";
             }
+
 
             if (cpu.registers.ProgrammCounter >= cpu.CountCommand)
                 nextExecuteCommandButton.IsEnabled = false;
@@ -109,10 +98,95 @@ namespace CpuGUI
 
         private void prevExecuteCommandButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
             cpu.ExcuteCommandPrev();
             cpu.registers.Flags[FlagsRegister.StepByStep] = true;
             flagsListBox.Items[5] = ($"{FlagsRegister.StepByStep}[True]");
+        }
+
+        private void intRegListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var curItem = intRegListBox.SelectedItem;
+            var index = intRegListBox.SelectedIndex;
+            if (index != -1 && superUser)
+                createChangeItemofListBoxWindow(curItem, index);
+
+
+        }
+
+        void createChangeItemofListBoxWindow(object item, int index)
+        {
+            Window w1 = new Window();
+
+            StackPanel g1 = new StackPanel();
+            TextBox txt1 = new TextBox();
+            Button btn1 = new Button();
+
+            void Btn1_Click(object sender, RoutedEventArgs e)
+            {
+
+                if (item is int)
+                {
+                    int num;
+                    if (int.TryParse(txt1.Text, out num))
+                    {
+                        cpu.registers.Integer[index] = num;
+                        intRegListBox.Items[index] = num;
+                        w1.Close();
+                    }
+                }
+                if (item is float)
+                {
+                    float num;
+                    if (float.TryParse(txt1.Text, out num))
+                    {
+                        cpu.registers.Float[index] = num;
+                        floatRegListBox.Items[index] = num;
+                        w1.Close();
+                    }
+                }
+
+            }
+
+
+
+            btn1.Height = 20;
+            btn1.Width = 75;
+            btn1.Margin = new Thickness(0, 5, 0, 5);
+            btn1.Content = "Ok";
+
+            txt1.Text = item.ToString();
+            txt1.Width = 200;
+            txt1.Height = 20;
+
+            g1.Children.Add(txt1);
+            g1.Children.Add(btn1);
+
+            w1.Title = $"change item {item}";
+            w1.Content = g1;
+            w1.Activate();
+            w1.Width = 300;
+            w1.Height = 100;
+            w1.Visibility = Visibility.Visible;
+
+            btn1.Click += Btn1_Click;
+
+
+        }
+
+        private void floatRegListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var curItem = floatRegListBox.SelectedItem;
+            var index = floatRegListBox.SelectedIndex;
+            if (index != -1 && superUser)
+                createChangeItemofListBoxWindow(curItem, index);
+        }
+
+        private void superUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            superUser = !superUser;
+            cpu.registers.Flags[FlagsRegister.SuperUser] = superUser;
+            UpdateRegView();s
         }
     }
 }
